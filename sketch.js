1b8cc0145e3f947db28cbb24b0518040ae0af7f3
BigT <theodorhhh@gmail.com>
2023-09-28 08:37:36 +0200

    Index

--- sketch.js
+++ sketch.js
@@ -1,7 +1,21 @@
-function setup() {
+let bobbel = [ ];
+let numBobbel = 5;
+let hspeed = 5;
+let lspeed = 2;
 
+function setup() {
+    createCanvas(400,400);
+
+    for(let i = 0; i < numBobbel; i++) {
+        bobbel.push(new Bobbel(random(20,width-20),y+random(lspeed,hspeed)))
+    }
 }
 
 function draw() {
-    
+    background(255);
+
+    for(let j = 0; j < bobbel.length; j++) {
+        bobbel[j].show();
+        bobbel[j].update();
+    }
 }
